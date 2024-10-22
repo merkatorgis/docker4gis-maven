ce93d0d75a0a9d43879acacba765c61d5f31b9c0
Azure Pipeline <pipeline@azure.com>
2024-10-22 12:42:03 +0200

    dg bump (--env-file)

--- run.sh
+++ run.sh
@@ -4,9 +4,6 @@ set -e
 src_dir=$1
 shift 1
 
-IMAGE=$IMAGE
-VOLUME=$VOLUME
-
 docker container run --rm \
 	--mount source="$VOLUME",target=/root/.m2 \
 	--mount type=bind,source="$src_dir",target=/src \
